787e3c77a7616644481128bb0aa348030f93b37b
Sky1sBloo <ultimate8gaming@gmail.com>
2023-11-04 07:36:49 +0800

    Added manage subscriptions

--- Wealth Wizard/DisplayForms/ManageSubscriptionsForm.Designer.cs
+++ Wealth Wizard/DisplayForms/ManageSubscriptionsForm.Designer.cs
@@ -30,21 +30,22 @@
         {
             this.Lbl_SubscriptionsDisplay = new System.Windows.Forms.Label();
             this.DataGridV_Subscriptions = new System.Windows.Forms.DataGridView();
-            this.Btn_Apply = new System.Windows.Forms.Button();
             this.Lbl_Date = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.DateT_SubDate = new System.Windows.Forms.DateTimePicker();
             this.Lbl_Type = new System.Windows.Forms.Label();
             this.ComboB_Type = new System.Windows.Forms.ComboBox();
             this.Lbl_Name = new System.Windows.Forms.Label();
             this.TxtB_EntryName = new System.Windows.Forms.TextBox();
             this.Lbl_BillingCycle = new System.Windows.Forms.Label();
             this.ComboB_BillingCycle = new System.Windows.Forms.ComboBox();
-            this.Btn_Cancel = new System.Windows.Forms.Button();
-            this.Btn_Ok = new System.Windows.Forms.Button();
+            this.Btn_Close = new System.Windows.Forms.Button();
             this.Btn_Delete = new System.Windows.Forms.Button();
             this.Btn_Edit = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Btn_AddSub = new System.Windows.Forms.Button();
+            this.Lbl_Amount = new System.Windows.Forms.Label();
+            this.NumTxtB_Amount = new NumericTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridV_Subscriptions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumTxtB_Amount)).BeginInit();
             this.SuspendLayout();
             // 
             // Lbl_SubscriptionsDisplay
@@ -68,15 +69,6 @@
             this.DataGridV_Subscriptions.Size = new System.Drawing.Size(502, 238);
             this.DataGridV_Subscriptions.TabIndex = 1;
             // 
-            // Btn_Apply
-            // 
-            this.Btn_Apply.Location = new System.Drawing.Point(636, 256);
-            this.Btn_Apply.Name = "Btn_Apply";
-            this.Btn_Apply.Size = new System.Drawing.Size(75, 23);
-            this.Btn_Apply.TabIndex = 2;
-            this.Btn_Apply.Text = "Apply";
-            this.Btn_Apply.UseVisualStyleBackColor = true;
-            // 
             // Lbl_Date
             // 
             this.Lbl_Date.AutoSize = true;
@@ -86,14 +78,14 @@
             this.Lbl_Date.TabIndex = 3;
             this.Lbl_Date.Text = "Date:";
             // 
-            // dateTimePicker1
+            // DateT_SubDate
             // 
-            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(49, 25);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(128, 20);
-            this.dateTimePicker1.TabIndex = 4;
+            this.DateT_SubDate.CustomFormat = "yyyy/MM/dd";
+            this.DateT_SubDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DateT_SubDate.Location = new System.Drawing.Point(49, 25);
+            this.DateT_SubDate.Name = "DateT_SubDate";
+            this.DateT_SubDate.Size = new System.Drawing.Size(128, 20);
+            this.DateT_SubDate.TabIndex = 4;
             // 
             // Lbl_Type
             // 
@@ -132,7 +124,7 @@
             // Lbl_BillingCycle
             // 
             this.Lbl_BillingCycle.AutoSize = true;
-            this.Lbl_BillingCycle.Location = new System.Drawing.Point(10, 109);
+            this.Lbl_BillingCycle.Location = new System.Drawing.Point(10, 136);
             this.Lbl_BillingCycle.Name = "Lbl_BillingCycle";
             this.Lbl_BillingCycle.Size = new System.Drawing.Size(66, 13);
             this.Lbl_BillingCycle.TabIndex = 9;
@@ -142,80 +134,97 @@
             // 
             this.ComboB_BillingCycle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.ComboB_BillingCycle.FormattingEnabled = true;
-            this.ComboB_BillingCycle.Location = new System.Drawing.Point(82, 103);
+            this.ComboB_BillingCycle.Location = new System.Drawing.Point(82, 130);
             this.ComboB_BillingCycle.Name = "ComboB_BillingCycle";
             this.ComboB_BillingCycle.Size = new System.Drawing.Size(95, 21);
             this.ComboB_BillingCycle.TabIndex = 10;
             // 
-            // Btn_Cancel
+            // Btn_Close
             // 
-            this.Btn_Cancel.Location = new System.Drawing.Point(555, 256);
-            this.Btn_Cancel.Name = "Btn_Cancel";
-            this.Btn_Cancel.Size = new System.Drawing.Size(75, 23);
-            this.Btn_Cancel.TabIndex = 11;
-            this.Btn_Cancel.Text = "Cancel";
-            this.Btn_Cancel.UseVisualStyleBackColor = true;
-            // 
-            // Btn_Ok
-            // 
-            this.Btn_Ok.Location = new System.Drawing.Point(474, 256);
-            this.Btn_Ok.Name = "Btn_Ok";
-            this.Btn_Ok.Size = new System.Drawing.Size(75, 23);
-            this.Btn_Ok.TabIndex = 12;
-            this.Btn_Ok.Text = "Ok";
-            this.Btn_Ok.UseVisualStyleBackColor = true;
+            this.Btn_Close.Location = new System.Drawing.Point(636, 257);
+            this.Btn_Close.Name = "Btn_Close";
+            this.Btn_Close.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Close.TabIndex = 11;
+            this.Btn_Close.Text = "Close";
+            this.Btn_Close.UseVisualStyleBackColor = true;
             // 
             // Btn_Delete
             // 
-            this.Btn_Delete.Location = new System.Drawing.Point(126, 130);
+            this.Btn_Delete.Location = new System.Drawing.Point(126, 157);
             this.Btn_Delete.Name = "Btn_Delete";
             this.Btn_Delete.Size = new System.Drawing.Size(51, 23);
             this.Btn_Delete.TabIndex = 13;
             this.Btn_Delete.Text = "Delete";
             this.Btn_Delete.UseVisualStyleBackColor = true;
+            this.Btn_Delete.Click += new System.EventHandler(this.Btn_Delete_Click);
             // 
             // Btn_Edit
             // 
-            this.Btn_Edit.Location = new System.Drawing.Point(70, 130);
+            this.Btn_Edit.Location = new System.Drawing.Point(70, 157);
             this.Btn_Edit.Name = "Btn_Edit";
             this.Btn_Edit.Size = new System.Drawing.Size(50, 23);
             this.Btn_Edit.TabIndex = 14;
             this.Btn_Edit.Text = "Edit";
             this.Btn_Edit.UseVisualStyleBackColor = true;
+            this.Btn_Edit.Click += new System.EventHandler(this.Btn_Edit_Click);
             // 
-            // button1
+            // Btn_AddSub
             // 
-            this.button1.Location = new System.Drawing.Point(13, 130);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(51, 23);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Btn_AddSub.Location = new System.Drawing.Point(13, 157);
+            this.Btn_AddSub.Name = "Btn_AddSub";
+            this.Btn_AddSub.Size = new System.Drawing.Size(51, 23);
+            this.Btn_AddSub.TabIndex = 15;
+            this.Btn_AddSub.Text = "Add";
+            this.Btn_AddSub.UseVisualStyleBackColor = true;
+            this.Btn_AddSub.Click += new System.EventHandler(this.Btn_AddSub_Click);
+            // 
+            // Lbl_Amount
+            // 
+            this.Lbl_Amount.AutoSize = true;
+            this.Lbl_Amount.Location = new System.Drawing.Point(10, 106);
+            this.Lbl_Amount.Name = "Lbl_Amount";
+            this.Lbl_Amount.Size = new System.Drawing.Size(46, 13);
+            this.Lbl_Amount.TabIndex = 16;
+            this.Lbl_Amount.Text = "Amount:";
+            // 
+            // NumTxtB_Amount
+            // 
+            this.NumTxtB_Amount.DecimalPlaces = 2;
+            this.NumTxtB_Amount.Location = new System.Drawing.Point(65, 104);
+            this.NumTxtB_Amount.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            0});
+            this.NumTxtB_Amount.Name = "NumTxtB_Amount";
+            this.NumTxtB_Amount.Size = new System.Drawing.Size(112, 20);
+            this.NumTxtB_Amount.TabIndex = 17;
             // 
             // ManageSubscriptionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(723, 292);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.NumTxtB_Amount);
+            this.Controls.Add(this.Lbl_Amount);
+            this.Controls.Add(this.Btn_AddSub);
             this.Controls.Add(this.Btn_Edit);
             this.Controls.Add(this.Btn_Delete);
-            this.Controls.Add(this.Btn_Ok);
-            this.Controls.Add(this.Btn_Cancel);
+            this.Controls.Add(this.Btn_Close);
             this.Controls.Add(this.ComboB_BillingCycle);
             this.Controls.Add(this.Lbl_BillingCycle);
             this.Controls.Add(this.TxtB_EntryName);
             this.Controls.Add(this.Lbl_Name);
             this.Controls.Add(this.ComboB_Type);
             this.Controls.Add(this.Lbl_Type);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.DateT_SubDate);
             this.Controls.Add(this.Lbl_Date);
-            this.Controls.Add(this.Btn_Apply);
             this.Controls.Add(this.DataGridV_Subscriptions);
             this.Controls.Add(this.Lbl_SubscriptionsDisplay);
             this.Name = "ManageSubscriptionsForm";
             this.Text = "Manage Subscriptions";
             ((System.ComponentModel.ISupportInitialize)(this.DataGridV_Subscriptions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumTxtB_Amount)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,19 +234,19 @@
 
         private System.Windows.Forms.Label Lbl_SubscriptionsDisplay;
         private System.Windows.Forms.DataGridView DataGridV_Subscriptions;
-        private System.Windows.Forms.Button Btn_Apply;
         private System.Windows.Forms.Label Lbl_Date;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker DateT_SubDate;
         private System.Windows.Forms.Label Lbl_Type;
         private System.Windows.Forms.ComboBox ComboB_Type;
         private System.Windows.Forms.Label Lbl_Name;
         private System.Windows.Forms.TextBox TxtB_EntryName;
         private System.Windows.Forms.Label Lbl_BillingCycle;
         private System.Windows.Forms.ComboBox ComboB_BillingCycle;
-        private System.Windows.Forms.Button Btn_Cancel;
-        private System.Windows.Forms.Button Btn_Ok;
+        private System.Windows.Forms.Button Btn_Close;
         private System.Windows.Forms.Button Btn_Delete;
         private System.Windows.Forms.Button Btn_Edit;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_AddSub;
+        private System.Windows.Forms.Label Lbl_Amount;
+        private NumericTextBox NumTxtB_Amount;
     }
 }
--- Wealth Wizard/DisplayForms/ManageSubscriptionsForm.cs
+++ Wealth Wizard/DisplayForms/ManageSubscriptionsForm.cs
@@ -19,7 +19,7 @@ namespace Wealth_Wizard.DisplayForms
 
             // Initialize table
             string[] columns = { "entry_date AS 'Start Date'", "entry_type AS 'Type'", "name AS 'Name'",
-                "amount AS 'Amount'", "billing_cycle AS 'Cycle'"};
+                "amount AS 'Amount'", "billing_cycle AS 'Billing Cycle'"};
             DataGridV_Subscriptions.DataSource = DatabaseHandler.GetValuesFromTable("subscriptions", columns);
 
             // Load Combo Box types
@@ -34,6 +34,38 @@ namespace Wealth_Wizard.DisplayForms
             { 
                 ComboB_BillingCycle.Items.Add(billingCycle);
             }
+            ComboB_BillingCycle.SelectedIndex = 0;
+        }
+
+        public void AddSubscriptionToTable(DateTime startDate, string entryType, string name, float amount, string billingCycle)
+        {
+            int rowIdx = DataGridV_Subscriptions.Rows.Add();
+        }
+
+        // Events
+        // Subscription buttons events
+        private void Btn_AddSub_Click(object sender, EventArgs e)
+        {
+            // Add new subscription to the table
+            
+            // Check if all forms have been completed
+            if (TxtB_EntryName.Text == "")
+            {
+                MessageBox.Show("Subscription Name field is empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            AddSubscriptionToTable(DateT_SubDate.Value, ComboB_Type.Text, TxtB_EntryName.Text,
+                (float)NumTxtB_Amount.Value, ComboB_BillingCycle.Text);
+        }
+
+        private void Btn_Edit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Delete_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
