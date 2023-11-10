60ebda973a23ecc955e64c20cca6d4af950f1edd
Sky1sBloo <ultimate8gaming@gmail.com>
2023-11-10 10:14:05 +0800

    TestDate.bat now moves up 1 folder rather than accessing from E:/

--- Testing/TestDate.bat
+++ Testing/TestDate.bat
@@ -4,4 +4,4 @@ set /p "Month=Month: "
 set /p "Day=Day: "
 
 setlocal
-START "" "E:\source\repos\Wealth Wizard\Wealth Wizard\bin\Debug\Wealth Wizard.exe" -setdate %Year% %Month% %Day%
+START "" "..\Wealth Wizard\bin\Debug\Wealth Wizard.exe" -setdate %Year% %Month% %Day%
